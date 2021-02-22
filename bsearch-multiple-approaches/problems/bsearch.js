2bc2255be9888d5b4c5622afb61f1af09d8db845
Keith Mellea <keithmellea@gmail.com>
2021-02-22 17:36:49 -0500

    first

--- bsearch-multiple-approaches/problems/bsearch.js
+++ bsearch-multiple-approaches/problems/bsearch.js
@@ -14,26 +14,40 @@ BINARY SEARCH VERSION 1:
 Write a Recursive Binary Search that returns a Boolean value indicating if
 targetNum is within the nums array.
 *******************************************************************/
-
+const oddNums = [11, 12, 13, 14, 15, 16, 17, 18, 19];
+const evenNums = [11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+const empty = [];
 const recurBSearch = (nums, targetNum) => {
   // Base Case: if nums has no length, return false because we've run out of 
   // items to search and haven't found targetNum
-
+      if (nums.length === 0) {
+        return false;
+      }
   // determine the slice point (ie the 'middle' of the array).
-
+      let middle = Math.floor(nums.length / 2);
+      // console.log(middle);
   // create "left half" and "right half" arrays, not including the slice point.
-
+      let leftHalf = nums.slice(0, middle);
+      let rightHalf = nums.slice(middle + 1);
   // if targetNum is less than the value in the array at slice point,
   // return this search on the left half
-
+        //if targetNum < nums[middle] return recurBleftHalf
+        if (targetNum < nums[middle]) {
+          return recurBSearch(leftHalf, targetNum);
+        }
   // if targetNum is greater than the value in the array at slice point,
   //return this search on the right half
-
+        else if (targetNum > nums[middle]) {
+          return recurBSearch(rightHalf, targetNum);
+        }
   // if it's not greater than or less than (i.e. 'else'),
   // we know it's equal so return true
+       else {
+         return true;
+       } 
 }
 
-
+ console.log(recurBSearch(evenNums, 25));
 /*******************************************************************
 BINARY SEARCH VERSION 2:
 
